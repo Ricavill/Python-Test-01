50df54627781bed3214c41324ce31c5e39be237e
rvillacis <ricavill@espol.edu.ec>
2025-03-13 13:07:37 -0500

    Se arregla conver_to_json

--- utils/json_utils.py
+++ utils/json_utils.py
@@ -1,9 +1,7 @@
 import json
 
-from config.error import ValidationException
 
-
-def convert_json_str_to_json(self, json_str):
+def convert_json_str_to_json(json_str):
     json_str = json_str.strip('```json').strip('```')
     try:
         json_dict = json.loads(json_str)
